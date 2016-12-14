70b1bdf273e71a1066452969357a75ecd280dc85
Serj Greensky <itpluspc@ya.ru>
2016-12-14 14:51:04 +0300

    UDP Client source code errors

--- udp_client.py
+++ udp_client.py
@@ -4,7 +4,7 @@ target_host = "127.0.0.1"
 target_port = 80
 
 # create a socket object
-client = socket.socket(socket.AF_INETm socket.SOCK_DGRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # send some data
 client.sendto("AAABBBCCC",(target_host,target_port))
